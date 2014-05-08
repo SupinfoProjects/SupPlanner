48ebadde9a846a3d4c3d6b79e8e234eb98a27b05
Wassim Dhif <wassimdhif@gmail.com>
2014-05-08 17:01:27 +0200

    - Ajout de la bdd SupPlanner.sql

---
+++ SupPlanner.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Jeu 08 Mai 2014 à 16:59
+-- Version du serveur: 5.5.37-0ubuntu0.14.04.1
+-- Version de PHP: 5.5.9-1ubuntu4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `SupPlanner`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nothing`
+--
+
+CREATE TABLE IF NOT EXISTS `nothing` (
+  `nothing` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
