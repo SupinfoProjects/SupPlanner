3bd4560a8e7a59538be46a6f23ee6d00f1003b72
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-05-09 12:44:50 +0200

    Update ToDo.java

--- src/ToDo.java
+++ src/ToDo.java
@@ -5,4 +5,13 @@ public abstract class ToDo {
     String authorName;
     Date startDate;
     Date endDate;
+
+    public ToDo(String _name, String _authorName, Date _startDate, Date _endDate) throws Exception {
+        this.name = _name;
+        this.authorName = _authorName;
+        this.startDate = _startDate;
+        this.endDate = _endDate;
+        if (this.startDate.after(this.endDate))
+            throw new Exception("Start date cannot be after end date");
+    }
 }
