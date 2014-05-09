20a24f0358b6c7852984d4e49ba8ecf0a0899709
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-05-09 12:46:54 +0200

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -2,10 +2,6 @@ import java.util.Date;
 
 public class Task extends ToDo
 {
-    private String name;
-    private String authorName;
-    private Date startDate;
-    private Date endDate;
     private boolean complete;
 
     public Task(String name, String authorName, Date startDate, Date endDate) throws Exception
