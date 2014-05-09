06cbbde99f2bba4ce2cfe328afb5c0eb19b92745
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-05-09 12:43:51 +0200

    Update Project.java

--- src/Project.java
+++ src/Project.java
@@ -1,6 +1,13 @@
-class Project extends ToDo
+import java.util.Date;
+
+public class Project extends ToDo
 {
     Task[] tasks;
+    int percentComplete;
 
-
+    public Project(String name, String authorName, Date startDate, Date endDate) throws Exception
+    {
+        super(name, authorName, startDate, endDate);
+        this.percentComplete = 0;
+    }
 }
