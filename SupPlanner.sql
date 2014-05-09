9d9cffe6f652de4b0c9d498877556a4828b09a38
Wassim Dhif <wassimdhif@gmail.com>
2014-05-09 12:36:46 +0200

    - Mise a jour de la BDD
    Wassim Dhif

--- SupPlanner.sql
+++ SupPlanner.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Jeu 08 Mai 2014 à 16:59
+-- Généré le: Ven 09 Mai 2014 à 12:34
 -- Version du serveur: 5.5.37-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4
 
@@ -23,11 +23,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `nothing`
+-- Structure de la table `project`
 --
 
-CREATE TABLE IF NOT EXISTS `nothing` (
-  `nothing` int(1) NOT NULL
+CREATE TABLE IF NOT EXISTS `project` (
+  `id` int(32) NOT NULL DEFAULT '0',
+  `name` varchar(64) DEFAULT NULL,
+  `completion` int(16) DEFAULT NULL,
+  `enumber` int(16) DEFAULT NULL,
+  `datebegin` date DEFAULT NULL,
+  `dateend` date DEFAULT NULL,
+  `link` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `task`
+--
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(16) NOT NULL DEFAULT '0',
+  `name` varchar(32) DEFAULT NULL,
+  `datebegin` date DEFAULT NULL,
+  `dateend` date DEFAULT NULL,
+  `author` varchar(32) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(16) NOT NULL DEFAULT '0',
+  `userType` varchar(16) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `projectid` int(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
