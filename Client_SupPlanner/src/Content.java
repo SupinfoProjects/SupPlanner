07bd5c13eb72b1243a7d5ec6e4e63eea3d201b4a
tyr-sl3 <tyr-sl3@users.noreply.github.com>
2014-05-14 22:06:33 +0200

    Update Content.java

--- Client_SupPlanner/src/Content.java
+++ Client_SupPlanner/src/Content.java
@@ -4,14 +4,18 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class Content extends JPanel {
-    public void paint(Graphics g){
-        try{
+public class Content extends JPanel
+{
+    public void paint(Graphics g)
+    {
+        try
+        {
             Image img = ImageIO.read(getClass().getResource("supPlanner.png"));
-            int middle = this.getWidth()/2 - img.getWidth(null)/2;
+            int middle = this.getWidth() / 2 - img.getWidth(null) / 2;
             g.drawImage(img, middle, 0, this);
         }
-        catch (IOException e){
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
